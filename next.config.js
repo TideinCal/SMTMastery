7195b709bdf83667955857e721eff105744bfba3
TideinCal <tideincalendar@gmail.com>
2025-08-26 16:13:36 -0700

    Enable static export for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true, // 👈 disables Next.js image optimization (necessary for static sites)
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
